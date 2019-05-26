003bfec63f1692e477bd2d85d4d4a208d2f2ec27
Lorena C <lorena@eunknown.org>
2019-05-26 04:31:32 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
-  # code goes here
+
+  attr_reader :name
+
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous" # You can initialize a behavior as a string
+  end
+
 end
